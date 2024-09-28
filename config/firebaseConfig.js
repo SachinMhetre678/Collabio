d39ad98386723aa27d1b3792110aac109c61158b
Sachin_Mhetre <138901374+SachinMhetre678@users.noreply.github.com>
2024-09-28 16:42:22 +0530

    Initial commit

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -3,11 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "collabio-e89c6.firebaseapp.com",
@@ -18,7 +14,13 @@ const firebaseConfig = {
   measurementId: "G-8MVEZPPSVF"
 };
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-const analytics = getAnalytics(app);
+let app; // Declare app variable
+
+if (typeof window !== 'undefined') {
+  // Firebase should only be initialized in the client environment
+  app = initializeApp(firebaseConfig);
+}
+
+// Export Firestore and Analytics conditionally
+export const db = typeof window !== 'undefined' ? getFirestore(app) : null;
+export const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null;
