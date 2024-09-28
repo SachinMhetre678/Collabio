9e1ae99206b1a24d5f800c39cad2076e38d269e9
Sachin_Mhetre <138901374+SachinMhetre678@users.noreply.github.com>
2024-09-28 14:26:51 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import { Button } from "../components/ui/button"; // Ensure this path is correct
 import Image from "next/image"; // If you're using images, ensure you're importing correctly
 import Hero from "./_components/Hero"; // Ensure the path to your Hero component is correct
 import Header from "./_components/Header"; // Ensure the path to your Header component is correct
-import AboutUs from './_components/AboutUs'; 
+import AboutUs from './_components/aboutUs'; 
 import Contact from './_components/Contact';
 
 export default function Home() {
